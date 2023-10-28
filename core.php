844adce949c81a60988efd20795c56b86028bd1d
vivianmediagroup <149206331+vivianmediagroup@users.noreply.github.com>
2023-10-27 20:24:34 -0400

    Create core.php

---
+++ core.php
@@ -0,0 +1,44 @@
+<?php
+// DO NOT EDIT UNLESS YOU KNOW EXACTLY WHAT YOU'RE DOING!
+$currentVer = 1;
+$githubUser = 'vivianmediagroup';
+
+$file_path = 'obsnewsstorage/currentVer.txt';
+if (file_exists($file_path)) {
+    $file_time = filemtime($file_path);
+    $current_time = time();
+    $time_difference = $current_time - $file_time;
+
+    if ($time_difference < 86400) {
+        $latestVer = file_get_contents('obsnewsstorage/currentVer.txt');
+    } else {
+        $latestVer = file_get_contents('https://cdn.jsdelivr.net/gh/'.$githubUser.'/obsNews-Update-System@main/latestVer.txt');
+		file_put_contents('obsnewsstorage/currentVer.txt', $latestVer);
+		$latestVer = file_get_contents('obsnewsstorage/currentVer.txt');
+    }
+} else {
+    $latestVer = file_get_contents('https://cdn.jsdelivr.net/gh/'.$githubUser.'/obsNews-Update-System@main/latestVer.txt');
+	file_put_contents('obsnewsstorage/currentVer.txt', $latestVer);
+	$latestVer = file_get_contents('obsnewsstorage/currentVer.txt');
+}
+
+$file_path = 'obsnewsstorage/fromObsNews.txt';
+if (file_exists($file_path)) {
+    $file_time = filemtime($file_path);
+    $current_time = time();
+    $time_difference = $current_time - $file_time;
+
+    if ($time_difference < 86400) {
+        $fromObsNews = file_get_contents('obsnewsstorage/fromObsNews.txt');
+    } else {
+        $fromObsNews = file_get_contents('https://cdn.jsdelivr.net/gh/'.$githubUser.'/obsNews-Update-System@main/fromObsNews.txt');
+		file_put_contents('obsnewsstorage/fromObsNews.txt', $fromObsNews);
+		$fromObsNews = file_get_contents('obsnewsstorage/fromObsNews.txt');
+    }
+} else {
+    $fromObsNews = file_get_contents('https://cdn.jsdelivr.net/gh/'.$githubUser.'/obsNews-Update-System@main/fromObsNews.txt');
+	file_put_contents('obsnewsstorage/fromObsNews.txt', $fromObsNews);
+	$fromObsNews = file_get_contents('obsnewsstorage/fromObsNews.txt');
+}
+
+?>
